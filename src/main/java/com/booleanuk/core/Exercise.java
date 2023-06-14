48e47bfd3a2efd77237ec96261b1edda5b943912
Dave Ames <david.john.ames@gmail.com>
2023-06-14 09:40:09 +0100

    Changed it to an enhanced switch which should fail.

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -2,21 +2,12 @@ package com.booleanuk.core;
 
 public class Exercise {
     public static String returnsMessage(String grade) {
-        String message = "";
-        switch (grade) {
-            case "A":
-                message = "You got an A";
-                break;
-            case "B":
-                message = "You got a B";
-                break;
-            case "C":
-                message = "You got a C";
-                break;
-            default:
-                message = "You didn't get an A or B";
-                break;
-        }
+        String message = switch (grade) {
+            case "A" -> "You got an A";
+            case "B" -> "You got a B";
+            case "C" -> "You got a C";
+            default -> "You didn't get an A or B";
+        };
         return message;
     }
 }
